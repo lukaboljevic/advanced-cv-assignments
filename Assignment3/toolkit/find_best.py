a7a73bdd5194ac9ed16db2a4202cec061d4218ed
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2023-04-17 20:50:24 +0200

    Update find_best.py

--- Assignment3/toolkit/find_best.py
+++ Assignment3/toolkit/find_best.py
@@ -1,45 +1,43 @@
 import os
 import json
 
-root_path = "../workspace-vot2014/analysis"
 
-all_trackers = []
+def show_top(array, n, type):
+    print(f"####### Sorted by {type} #######\n")
+    for i in range(n):
+        curr = array[i]
+        print(f"Rank {i+1}")
+        print(f"   Name: {curr['tracker_name']}")
+        print(f"   Failures: {curr['total_failures']}")
+        print(f"   Overlap: {curr['average_overlap']}")
+        print(f"   Average speed: {(curr['average_speed']):.2f} FPS")
+        print(f"   Average init speed: {(curr['average_init_speed']):.2f} FPS")
+        print()
+    print("--------------------------------------------------")
+    print("--------------------------------------------------")
 
-for tracker_dir in os.listdir(root_path):
-    results_json_path = os.path.join(root_path, tracker_dir, "results.json")
-    with open(results_json_path, "r") as f:
-        data = json.load(f)
 
-    # curr_tracker_name = data["tracker_name"]
-    # enlarge_factor = float(curr_tracker_name.split("-")[1][:-2])
-    # if enlarge_factor != 1.0:
-    #     continue
+if __name__ == "__main__":
+    root_path = "../workspace-vot2014/analysis"
+    all_trackers = []
 
-    all_trackers.append(data)
+    for tracker_dir in os.listdir(root_path):
+        results_json_path = os.path.join(root_path, tracker_dir, "results.json")
+        with open(results_json_path, "r") as f:
+            data = json.load(f)
 
-by_failures = sorted(all_trackers, key=lambda tracker: tracker["total_failures"])
-by_overlap = sorted(all_trackers, key=lambda tracker: tracker["average_overlap"], reverse=True)
+        # curr_tracker_name: str = data["tracker_name"]
+        # if curr_tracker_name.find("particle") < 0:
+        #     continue
 
-n = 3
+        all_trackers.append(data)
 
-for i in range(n):
-    curr = by_failures[i]
-    print(f"Rank {i+1}")
-    print(f"   Name: {curr['tracker_name']}")
-    print(f"   Failures: {curr['total_failures']}")
-    print(f"   Overlap: {curr['average_overlap']}")
-    print(f"   Average speed: {(curr['average_speed']):.2f} FPS")
-    print(f"   Average init speed: {(curr['average_init_speed']):.2f} FPS")
-    print()
+    by_failures = sorted(all_trackers, key=lambda tracker: tracker["total_failures"])
+    by_overlap = sorted(all_trackers, key=lambda tracker: tracker["average_overlap"], reverse=True)
+    by_speed = sorted(all_trackers, key=lambda tracker: tracker["average_speed"], reverse=True)
 
-print("--------------------------------------------------")
-print("--------------------------------------------------\n")
+    n = 5
 
-for i in range(n):
-    curr = by_overlap[i]
-    print(f"Rank {i+1}")
-    print(f"   Name: {curr['tracker_name']}")
-    print(f"   Failures: {curr['total_failures']}")
-    print(f"   Overlap: {curr['average_overlap']}")
-    print(f"   Average speed: {(curr['average_speed']):.2f} FPS")
-    print(f"   Average init speed: {(curr['average_init_speed']):.2f} FPS")
+    show_top(by_failures, n, "failures")
+    show_top(by_overlap, n, "overlap")
+    show_top(by_speed, n, "speed")
